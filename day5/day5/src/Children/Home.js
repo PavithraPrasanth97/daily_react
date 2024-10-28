48d2b715ca25af22fc446bbd6ae9dd419d14bb26
PavithraPrasanth97 <pavithra19041997@gmail.com>
2024-10-28 17:18:57 +0530

    Unwanted files deleted

--- day5/day5/src/Children/Home.js
+++
@@ -1,9 +0,0 @@
-import React, {useContext} from 'react'
-import Usercontext from './MainRouting'
-
-export const Home = () => {
-    const {name} = useContext(Usercontext)
-  return (
-    <div>Home {name}</div>
-  )
-}
